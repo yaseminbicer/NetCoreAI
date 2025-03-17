11a13e4ceeca18878fcdff0267dea812e011c83e
Yasemin Biçer <yasemin.bicer@bahcesehir.edu.tr>
2025-03-17 14:59:02 +0300

    Rapid Api

---
+++ NetCoreAI.Project03_RapidApi/Program.cs
@@ -0,0 +1,28 @@
+﻿using NetCoreAI.Project03_RapidApi.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text.Json.Serialization;
+var client = new HttpClient();
+
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+    Headers =
+    {
+        { "x-rapidapi-key", "4e3944a894msh58c946b06d85a6fp1638cbjsn2350d7fdfa10" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    apiSeriesViewModels=JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+    foreach(var series in apiSeriesViewModels)
+    {
+        Console.WriteLine(series.rank + "-" + series.title +"-Film Puanı"+ series.rating + "-Yapım Yılı" + series.year);
+    }
+}
+Console.ReadLine();
